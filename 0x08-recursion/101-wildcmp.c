9f551aef59ea7980dd5e68b2cf13e19f7dfedce5
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-08-03 13:14:08 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,14 +11,16 @@ if (*s2 == '*')
 {
 if (*(s2 + 1) == '*')
 return (wildcmp(s1, s2 + 1));
-else if (wildcmp(s1, s2 + 1))
+else if (*s1 == '\0')
+return (wildcmp(s1, s2 + 1));
+else if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 return (1);
 else
-return (wildcmp(s1 + 1, s2));
+return (0);
 }
-else if (*s2 == '\0')
+else if (*s1 == '\0' || *s2 == '\0')
 {
-if(*s1 == '\0')
+if(*s1 == *s2)
 return (1);
 else
 return (0);

e9de6f7af874595c9a184a5b611ff8a391c19e38
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-08-03 12:41:04 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,7 +6,7 @@
  */
 int _strlen_recursion(char *s)
 {
-if (*s == 0)
+if (*s == '\0')
 return (0);
 else
 return (1 + _strlen_recursion(s + 1));
@@ -20,13 +20,11 @@ return (1 + _strlen_recursion(s + 1));
  */
 int _chunk_ispal(char *s, int start, int end)
 {
-if (start == end)
+if (start >= end)
 return (1);
 if (s[start] != s[end])
 return (0);
-if (start < end + 1)
 return (_chunk_ispal(s, start + 1, end - 1));
-return (0);
 }
 /**
  * is_palindrome - Tests if a string is a palindrome.
@@ -36,7 +34,7 @@ return (0);
 int is_palindrome(char *s)
 {
 int n = _strlen_recursion(s);
-if (n == 0)
+if (n <= 0)
 return (1);
 return (_chunk_ispal(s, 0, n - 1));
 }

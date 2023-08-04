ca32343a47bf8565fd9aee1f98cbbc147dfae12d
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-08-04 08:29:53 -0700

    arc,argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: The number of passed to the program
+ * @argv: An array of strings containing the arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}

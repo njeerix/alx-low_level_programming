aede6ecb46d6ad12aa7906e163a5507bcaa10c3e
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-08-04 08:15:01 -0700

    arc,argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}

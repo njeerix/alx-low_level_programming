84b34c00584d588cc24398674ef1aa89789b0981
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-08-10 04:18:34 -0700

    mare malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,24 +9,22 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int len1 = 0, len2 = 0, i, j;
+unsigned int len = n, index;
 char *concat;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-while (s1[len1])
-len1++;
-while (s2[len2])
-len2++;
-if (n >= len2)
-n = len2;
-concat = malloc(sizeof(char) * (len1 + n + 1));
+for (index = 0; s1[index]; index++)
+len++;
+concat = malloc(sizeof(char) * (len + 1));
 if (concat == NULL)
 return (NULL);
-for (i = 0; i < len1; i++)
-concat[i] = s1[i];
-for (j = 0; j < n; j++)
-concat[i + j] = '\0';
+len = 0;
+for (index = 0; s1[index]; index++)
+concat[len++] = s1[index];
+for (index = 0; s2[index] && index < n; index++)
+concat[len++] = s2[index];
+concat[len] = '\0';
 return (concat);
 }

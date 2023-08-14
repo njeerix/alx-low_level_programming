8210b370c48f5bc3e11ca3ccc5e95cd42744ab93
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-08-14 13:46:25 -0700

    typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * new_dog - Creates a new dog.
+ * @name: Pointer to the name value.
+ * @age: Float value for the age.
+ * @owner: Pointer to the owner value.
+ * Return: Pointer to the new dog structure.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *new_dog;
+if (name == NULL || owner == NULL)
+return (NULL);
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+new_dog->name = strdup(name);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return(NULL);
+}
+new_dog->age = age;
+new_dog->owner = strdup(owner);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+return (new_dog);
+}

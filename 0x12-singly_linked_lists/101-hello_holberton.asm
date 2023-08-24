a34373043c3d09e9d1d1b9ddf9cb537c13a18cee
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-08-24 09:21:36 -0700

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,13 @@
 SECTION .data
 msg db "Hello, Holberton", 0
 fmt db "%s", 10, 0
-
 SECTION .text
 extern printf
 global main
-
 main:
 mov esi, msg
 mov adi, fmt
 mov eax, 0
 call printf
-
 mov eax, 0
 ret

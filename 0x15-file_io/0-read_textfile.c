5c85f1f6a0b634f4b26c3f3d41ad8dc36025c72d
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-09-04 08:23:08 -0700

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,26 +16,35 @@ int fd;
 ssize_t n_read, n_written;
 char *buffer;
 if (filename == NULL)
+{
+perror("read_textfile");
 return (0);
+}
 fd = open(filename, O_RDONLY);
 if (fd == -1)
+{
+perror("read_textfile");
 return (0);
+}
 buffer = (char *)malloc(sizeof(char) * letters);
 if (buffer == NULL)
 {
+perror("read_textfile");
 close(fd);
 return (0);
 }
 n_read = read(fd, buffer, letters);
 if (n_read == -1)
 {
+perror("read_textfile");
 free(buffer);
 close(fd);
 return (0);
 }
 n_written = write(STDOUT_FILENO, buffer, n_read);
-if (n_written == -1 || (ssize_t)n_written != n_read)
+if (n_written == -1 || n_written != n_read)
 {
+perror("read_textfile");
 free(buffer);
 close(fd);
 return (0);

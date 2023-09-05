c4289f171b31914e98671917741322dfeceb0d18
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-09-05 04:27:33 -0700

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
 /**
  * read_textfile - Reads and prints a text file to the POSIX
@@ -15,41 +15,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 int fd;
 ssize_t n_read, n_written;
 char *buffer;
-if (filename == NULL)
+if (!filename)
 {
-perror("read_textfile");
+dprintf(STDERR_FILENO, "read_textfile: No such file or directory\n");
 return (0);
 }
 fd = open(filename, O_RDONLY);
 if (fd == -1)
-{
-perror("read_textfile");
 return (0);
-}
-buffer = (char *)malloc(sizeof(char) * letters);
-if (buffer == NULL)
-{
-perror("read_textfile");
-close(fd);
+buffer = malloc(sizeof(char) * (letters));
+if (!buffer)
 return (0);
-}
 n_read = read(fd, buffer, letters);
-if (n_read == -1)
-{
-perror("read_textfile");
-free(buffer);
-close(fd);
-return (0);
-}
 n_written = write(STDOUT_FILENO, buffer, n_read);
-if (n_written == -1 || n_written != n_read)
-{
-perror("read_textfile");
-free(buffer);
 close(fd);
-return (0);
-}
 free(buffer);
-close(fd);
-return (n_read);
+return (n_written);
 }

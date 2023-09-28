f008968f36eb672e9828c773468a6ff5a2581466
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-09-28 07:52:57 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,18 +7,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int result = 0;
-unsigned int power = 1;
 if (b == NULL)
 return (0);
-while (b && *b)
-b++;
-while (--b)
+while (*b)
 {
 if (*b != '0' && *b != '1')
 return (0);
-if (*b == '1')
-result += power;
-power *= 2;
+result = (result << 1) + (*b - '0');
+b++;
 }
 return (result);
 }

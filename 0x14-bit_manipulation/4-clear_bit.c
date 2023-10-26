6d0d0cc86d7c2a1a0f02a7c18b8caa5b7832a1d3
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-10-26 08:52:11 -0700

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,11 +8,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+unsigned long int mask;
 if (n == NULL || index >= sizeof(unsigned long int) * 8)
 return (-1);
-unsigned long int mask = 1;
-mask <<= index;
-mask = -mask;
-*n &= mask;
+mask = 1 << index;
+*n &= ~mask;
 return (1);
 }

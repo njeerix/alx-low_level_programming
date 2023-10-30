5fbc628e6de81657c4fee93a877c85b0d0fb147d
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-10-30 04:18:31 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,49 @@
 #include <unistd.h>
 #include <fcntl.h>
 /**
+ * open_file - Opensa file with specified flags and mode.
+ * @filename: the name of the file to open.
+ * @flags: flags to use when opening the file.
+ * @mode: mode to set when creating a new file.
+ * Return: the file descriptor or -1 on error.
+ */
+int open_file(const char *filename, int flags, mode_t mode)
+{
+int fd = open(filename, flags, mode);
+if (fd == -1)
+{
+fprintf(stderr, "Error: Can't open file %s\n", filename);
+}
+return (fd);
+}
+/**
+ * copy_file - copies the content of one file to another.
+ * @fd_from: file descriptor of the source file.
+ * @fd_to: file descriptor of the destination file.
+ * Return: 0 on success, 1 on error.
+ */
+int copy_file(int fd_from, int fd_to)
+{
+ssize_t bytes_read;
+ssize_t bytes_written;
+char buffer[1024];
+while ((bytes_read = read(fd_from, buffer, sizeof(buffer)) > 0))
+{
+bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+fprintf(stderr, "Error: Can't write to file\n");
+return (1);
+}
+}
+if (bytes_read == -1)
+{
+fprintf(stderr, "Error: Can't read from file\n");
+return (1);
+}
+return (0);
+}
+/**
 * main - Entry point
 * @argc: The number of arguments.
 * @argv: The array of arguments
@@ -10,54 +53,35 @@
 */
 int main(int argc, char **argv)
 {
-int fd_form, fd_to, bytes_read, bytes_written;
-char buffer[1024];
+int copy_result;
+int fd_from, fd_to;
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+fprintf(stderr, "Usage: cp file_from file_to\n");
+return (97);
 }
-fd_form = open(argv[1], O_RDONLY);
-if (fd_form == -1)
+fd_from = open(argv[1], O_RDONLY, 0);
+if (fd_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 return (98);
 }
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRUSR
-| S_IRGRP | S_IWGRP | S_IROTH);
+fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+S_IRUSR | S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (fd_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(fd_form);
+close(fd_from);
 return (99);
 }
-while ((bytes_read = read(fd_form, buffer, sizeof(buffer))) > 0)
+copy_result = copy_file(fd_from, fd_to);
+if (close(fd_from) == -1)
 {
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(fd_form);
-close(fd_to);
-return (99);
-}
-}
-if (bytes_read == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-close(fd_form);
-close(fd_to);
-return (98);
-}
-if (close(fd_form) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_form);
+fprintf(stderr, "Error: Can't close fd %d\n", fd_from);
 return (100);
 }
 if (close(fd_to) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+fprintf(stderr, "Error: Can't close fd %d\n", fd_to);
 return (100);
 }
-return (0);
+return (copy_result);
 }

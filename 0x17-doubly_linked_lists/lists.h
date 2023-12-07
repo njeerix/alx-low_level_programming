0a4bf9ea9e4caa6b6187109a76fabe8d314ff144
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-12-07 06:38:25 -0800

    Doubly linked lists

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -11,5 +11,6 @@ dlistint_t;
 int main(void);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 size_t print_dlistint(const dlistint_t *h);
 #endif

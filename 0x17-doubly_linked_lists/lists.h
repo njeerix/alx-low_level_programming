8bf59f75cbdb28f4c903192e97683de03863b91b
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-12-07 07:32:54 -0800

    Doubly linked lists

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -16,5 +16,6 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 size_t print_dlistint(const dlistint_t *h);
 #endif

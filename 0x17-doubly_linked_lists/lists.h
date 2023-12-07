72d966b4dba74eb644ffbbd9124e3fa3313e3a67
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-12-07 06:10:28 -0800

    Doubly linked lists

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,13 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
+typedef struct dlistint_s
+{
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
+}
+dlistint_t;
+int main(void);
+size_t print_dlistint(const dlistint_t *h);
+#endif
